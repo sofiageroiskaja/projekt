db830a419016d9afe86b2704a2af47242e4b91c1
sofia <sofiageroiskaja@users.noreply.github.com>
2021-05-16 15:43:15 +0300

    index on tehtud

---
+++ javascript/kalkulaator.js
@@ -0,0 +1,16 @@
+function IndexToEng() {
+    document.getElementById('pealkiri').innerHTML = "Curriculum scenario calculator";
+    document.getElementById('pealeht').innerHTML = "Back to mainpage";
+    document.getElementById('green_line').innerHTML = "Study data can be found in Õis under study results";
+    document.getElementById('oppekavad_label').innerHTML = "Choose a curriculum";
+    document.getElementById('vali_oppekava').style.marginLeft = "45px";
+    document.getElementById('tasuta_label').innerHTML = "Free";
+    document.getElementById('tasuline_label').innerHTML = "Paid";
+    document.getElementById('tasuline_label').style.marginLeft = "13px";
+    document.getElementById('edasi').innerText = "Next";
+    document.getElementById('edasi').style.color = "white";
+    document.getElementById('placeholder').innerHTML = "Choose a curriculum...";
+    document.getElementById('informaatika').innerHTML = "Informatics";
+    document.getElementById('infotehnoloogia').innerHTML = "Infotechnology";
+    document.getElementById('infoteadus').innerHTML = "information science";
+}
